19f2e093123b8ca2ec8892b4e731df460ddc885e
Jens Vesti <jve@mjolner.dk>
2021-12-06 15:17:15 +0100

    #64 #71 fixed

--- app/src/main/java/com/concordium/wallet/ui/transaction/transactiondetails/TransactionDetailsViewModel.kt
+++ app/src/main/java/com/concordium/wallet/ui/transaction/transactiondetails/TransactionDetailsViewModel.kt
@@ -8,6 +8,7 @@ import com.concordium.wallet.App
 import com.concordium.wallet.R
 import com.concordium.wallet.core.arch.Event
 import com.concordium.wallet.core.backend.BackendRequest
+import com.concordium.wallet.data.RecipientRepository
 import com.concordium.wallet.data.TransferRepository
 import com.concordium.wallet.data.backend.repository.ProxyRepository
 import com.concordium.wallet.data.model.Transaction
@@ -18,11 +19,14 @@ import com.concordium.wallet.data.room.Account
 import com.concordium.wallet.data.room.WalletDatabase
 import com.concordium.wallet.ui.common.BackendErrorHandler
 import com.concordium.wallet.util.Log
+import kotlinx.coroutines.runBlocking
 
 class TransactionDetailsViewModel(application: Application) : AndroidViewModel(application) {
 
     var isShieldedAccount: Boolean = false
     private val proxyRepository = ProxyRepository()
+    private val recipientRepository: RecipientRepository
+
     private val transferRepository: TransferRepository
     private val gson = App.appCore.gson
 
@@ -45,6 +49,9 @@ class TransactionDetailsViewModel(application: Application) : AndroidViewModel(a
     init {
         val transferDao = WalletDatabase.getDatabase(application).transferDao()
         transferRepository = TransferRepository(transferDao)
+        val recipientDao = WalletDatabase.getDatabase(application).recipientDao()
+        recipientRepository = RecipientRepository(recipientDao)
+
     }
 
     fun initialize(account: Account, transaction: Transaction) {
@@ -92,10 +99,20 @@ class TransactionDetailsViewModel(application: Application) : AndroidViewModel(a
                 _errorLiveData.value = Event(BackendErrorHandler.getExceptionStringRes(it))
             }
         )
+
     }
 
     fun setIsShieldedAccount(shielded: Boolean) {
         isShieldedAccount  = shielded
+    }
+
+    fun addressLookup(address: String?, defaultTitle: String?): Any? {
+        if(address == null){
+            return defaultTitle
+        }
+        return runBlocking {
+            recipientRepository.getRecipientByAddress(address)?.let { it.name }
+        } ?: defaultTitle
     }
 
 }
