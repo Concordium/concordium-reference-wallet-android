7dc6f8b7a8e7b6341b85b63530f009ad048c98cd
Lars Christensen <81175980+XOOPsoft@users.noreply.github.com>
2022-05-11 20:29:43 +0200

    Check for closed pool at register delegation with baker pool

--- app/src/main/java/com/concordium/wallet/ui/bakerdelegation/common/DelegationBakerViewModel.kt
+++ app/src/main/java/com/concordium/wallet/ui/bakerdelegation/common/DelegationBakerViewModel.kt
@@ -216,7 +216,7 @@ class DelegationBakerViewModel(application: Application) : AndroidViewModel(appl
                 {
                     bakerDelegationData.bakerPoolStatus = it
                     _waitingLiveData.value = false
-                    if (bakerDelegationData.bakerPoolStatus?.poolInfo?.openStatus == BakerPoolInfo.OPEN_STATUS_CLOSED_FOR_NEW)
+                    if (bakerDelegationData.bakerPoolStatus?.poolInfo?.openStatus == BakerPoolInfo.OPEN_STATUS_CLOSED_FOR_NEW || bakerDelegationData.bakerPoolStatus?.poolInfo?.openStatus == BakerPoolInfo.OPEN_STATUS_CLOSED_FOR_ALL)
                         _errorLiveData.value = Event(R.string.delegation_register_delegation_pool_id_closed)
                     else if (bakerDelegationData.account?.accountDelegation?.stakedAmount?.toLong() ?: 0 > bakerDelegationData.bakerPoolStatus?.delegatedCapitalCap?.toLong() ?: 0)
                         _errorLiveData.value = Event(AMOUNT_TOO_LARGE_FOR_POOL)
