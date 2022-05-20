b1873d66325df7d94bfea33eb417c1eb168534e2
Lars Christensen <81175980+XOOPsoft@users.noreply.github.com>
2022-05-20 10:26:26 +0200

    Bugfix issue 155 (total amount on send funds)

--- app/src/main/java/com/concordium/wallet/ui/transaction/sendfunds/SendFundsActivity.kt
+++ app/src/main/java/com/concordium/wallet/ui/transaction/sendfunds/SendFundsActivity.kt
@@ -369,7 +369,7 @@ class SendFundsActivity :
         else{
             balance_total_text.text = getString(R.string.accounts_overview_account_total)
             at_disposal_total_text.text = getString(R.string.accounts_overview_at_disposal)
-            balance_total_textview.text = CurrencyUtil.formatGTU(viewModel.account.totalUnshieldedBalance - viewModel.account.getAtDisposalSubstraction(), withGStroke = true)
+            balance_total_textview.text = CurrencyUtil.formatGTU(viewModel.account.totalUnshieldedBalance, withGStroke = true)
             at_disposal_total_textview.text = CurrencyUtil.formatGTU(viewModel.account.totalUnshieldedBalance - viewModel.account.getAtDisposalSubstraction(), withGStroke = true)
         }
 
