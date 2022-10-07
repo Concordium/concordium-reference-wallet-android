13f8f5691813cf34c086a2de4f0bd0febfb9e634
Lars Christensen <81175980+XOOPsoft@users.noreply.github.com>
2022-10-07 22:43:45 +0700

    Search tokens view

--- app/src/main/java/com/concordium/wallet/ui/cis2/FindTokensBottomSheet.kt
+++ app/src/main/java/com/concordium/wallet/ui/cis2/FindTokensBottomSheet.kt
@@ -2,15 +2,28 @@ package com.concordium.wallet.ui.cis2
 
 import android.app.Activity
 import android.content.res.Resources
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.SearchView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.concordium.wallet.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.coroutines.*
 
 class FindTokensBottomSheet(private var activity: Activity) {
     private lateinit var dialog: BottomSheetDialog
+    private lateinit var title: TextView
+    private lateinit var contractAddress: EditText
+    private lateinit var pending: AppCompatImageView
+    private lateinit var button: Button
+    private lateinit var error: TextView
+    private lateinit var search: SearchView
+    private lateinit var tokenFound: ListView
 
     fun initialize() {
         val height = (Resources.getSystem().displayMetrics.heightPixels * 0.95).toInt()
@@ -26,16 +39,74 @@ class FindTokensBottomSheet(private var activity: Activity) {
         params?.height = height
         content?.layoutParams = params
 
-        val title = dialog.findViewById<TextView>(R.id.title)
-        title?.text = activity.getString(R.string.cis_find_tokens_title)
+        dialog.findViewById<TextView>(R.id.title)?.let {
+            title = it
+        }
+        dialog.findViewById<EditText>(R.id.contract_address)?.let {
+            contractAddress = it
+        }
+        dialog.findViewById<AppCompatImageView>(R.id.pending)?.let {
+            pending = it
+        }
+        dialog.findViewById<Button>(R.id.look)?.let {
+            button = it
+        }
+        dialog.findViewById<TextView>(R.id.error)?.let {
+            error = it
+        }
+        dialog.findViewById<SearchView>(R.id.search)?.let {
+            search = it
+        }
+        dialog.findViewById<ListView>(R.id.tokens_found)?.let {
+            tokenFound = it
+        }
 
-        val look = dialog.findViewById<Button>(R.id.look)
-        look?.setOnClickListener {
+        title.text = activity.getString(R.string.cis_find_tokens_title)
 
+        button.setOnClickListener {
+            button.isEnabled = false
+            contractAddress.isEnabled = false
+            pending.visibility = View.VISIBLE
+            error.visibility = View.GONE
+            look()
         }
     }
 
     fun show() {
         dialog.show()
+    }
+
+    private fun look() {
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            activity.runOnUiThread {
+                handleLookup()
+            }
+        }
+    }
+
+    private fun handleLookup() {
+        val ok = true
+        button.isEnabled = true
+        pending.visibility = View.GONE
+        if (ok) {
+            title.text = activity.getString(R.string.cis_select_tokens_title)
+            contractAddress.visibility = View.GONE
+            search.visibility = View.VISIBLE
+            tokenFound.visibility = View.VISIBLE
+            button.text = activity.getString(R.string.cis_add_tokens)
+            button.setOnClickListener {
+                addTokens()
+            }
+        } else {
+            contractAddress.setTextColor(activity.getColor(R.color.text_pink))
+            contractAddress.setBackgroundResource(R.drawable.rounded_pink)
+            //contractAddress.onEdit
+            error.visibility = View.VISIBLE
+        }
+    }
+// https://www.digitalocean.com/community/tutorials/android-searchview-example-tutorial
+    private fun addTokens() {
+
     }
 }
