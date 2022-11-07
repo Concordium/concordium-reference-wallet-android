dc3f088a865a5cdd61146b98011593f2dd091364
Lars Christensen <81175980+XOOPsoft@users.noreply.github.com>
2022-11-07 13:27:57 +0100

    Database migration

--- app/src/main/java/com/concordium/wallet/data/room/WalletDatabase.kt
+++ app/src/main/java/com/concordium/wallet/data/room/WalletDatabase.kt
@@ -1,19 +1,17 @@
 package com.concordium.wallet.data.room
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.*
 import com.concordium.wallet.data.room.WalletDatabase.Companion.VERSION_NUMBER
 import com.concordium.wallet.data.room.typeconverter.GlobalTypeConverters
 
 @Database(
-    entities = arrayOf(Identity::class, Account::class, Transfer::class, Recipient::class, EncryptedAmount::class, AccountContract::class, ContractToken::class),
+    entities = [Identity::class, Account::class, Transfer::class, Recipient::class, EncryptedAmount::class, AccountContract::class, ContractToken::class],
     version = VERSION_NUMBER,
-    exportSchema = true
+    exportSchema = true,
+    autoMigrations = [
+        AutoMigration (from = 7, to = 8)
+    ]
 )
 @TypeConverters(GlobalTypeConverters::class)
 abstract class WalletDatabase : RoomDatabase() {
@@ -27,39 +25,7 @@ abstract class WalletDatabase : RoomDatabase() {
     abstract fun contractTokenDao(): ContractTokenDao
 
     companion object {
-
         const val VERSION_NUMBER = 8
-
-        val MIGRATION_3_4: Migration = object : Migration(3, 4) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE transfer_table ADD COLUMN memo TEXT")
-            }
-        }
-
-        val MIGRATION_4_5: Migration = object : Migration(4, 5) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE account_table ADD COLUMN account_delegation TEXT")
-            }
-        }
-
-        val MIGRATION_5_6: Migration = object : Migration(5, 6) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE account_table ADD COLUMN account_baker TEXT")
-            }
-        }
-
-        val MIGRATION_6_7: Migration = object : Migration(6, 7) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE account_table ADD COLUMN accountIndex INTEGER")
-            }
-        }
-
-        val MIGRATION_7_8: Migration = object : Migration(7, 8) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("CREATE TABLE IF NOT EXISTS 'account_contract_table' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'account_address' STRING, 'contract_index' STRING)")
-                database.execSQL("CREATE TABLE IF NOT EXISTS 'contract_token_table' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'contract_index' STRING, 'token_id' INTEGER)")
-            }
-        }
 
         // Singleton prevents multiple instances of database opening at the same time.
         @Volatile
@@ -75,13 +41,7 @@ abstract class WalletDatabase : RoomDatabase() {
                     context.applicationContext,
                     WalletDatabase::class.java,
                     "wallet_database"
-                )
-                    .addMigrations(MIGRATION_3_4)
-                    .addMigrations(MIGRATION_4_5)
-                    .addMigrations(MIGRATION_5_6)
-                    .addMigrations(MIGRATION_6_7)
-                    .addMigrations(MIGRATION_7_8)
-                    .build()
+                ).build()
                 INSTANCE = instance
                 return instance
             }
