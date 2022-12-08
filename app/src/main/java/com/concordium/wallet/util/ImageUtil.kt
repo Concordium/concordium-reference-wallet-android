ea027b4ef0cf41fbd85efad60a8252dbf7d26cbc
Lars Christensen <81175980+XOOPsoft@users.noreply.github.com>
2022-12-08 07:41:31 +0100

    CBW-599 - New identity card style

--- app/src/main/java/com/concordium/wallet/util/ImageUtil.kt
+++ app/src/main/java/com/concordium/wallet/util/ImageUtil.kt
@@ -3,22 +3,10 @@ package com.concordium.wallet.util
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
 
 object ImageUtil {
-
     fun getImageBitmap(image: String): Bitmap {
         val imageBytes = Base64.decode(image, Base64.DEFAULT)
-        return BitmapFactory.decodeByteArray(
-            imageBytes, 0, imageBytes.size
-        )
-    }
-
-    fun changeImageViewTintColor(imageView: ImageView, color: Int) {
-        imageView.setColorFilter(
-            ContextCompat.getColor(imageView.context, color),
-            android.graphics.PorterDuff.Mode.MULTIPLY
-        )
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
     }
 }
