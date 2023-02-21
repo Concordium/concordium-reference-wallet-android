df899873cc61bc524742cc955060f71de089d047
Sasho <sasho@axellero.com>
2023-02-21 11:50:46 +0100

    Schema string to object.

--- app/src/main/java/com/concordium/wallet/ui/walletconnect/WalletConnectViewModel.kt
+++ app/src/main/java/com/concordium/wallet/ui/walletconnect/WalletConnectViewModel.kt
@@ -296,14 +296,15 @@ class WalletConnectViewModel(application: Application) : AndroidViewModel(applic
 
         binder?.getSessionRequestParams()?.let { params ->
             params.payloadObj = params.parsePayload()
+            params.schemaObj = params.parseSchema()
             params.payload = ""
-            if (params.payloadObj != null && params.payloadObj?.message != null && params.schema != null) {
+            if (params.payloadObj != null && params.payloadObj?.message != null && params.schemaObj != null) {
                 CoroutineScope(Dispatchers.IO).launch {
                     val jsonMessage = App.appCore.cryptoLibrary.parameterToJson(
                         ParameterToJsonInput(
                             params.payloadObj!!.message,
                             params.payloadObj!!.receiveName,
-                            params.schema!!,
+                            params.schemaObj!!,
                             null
                         )
                     )
