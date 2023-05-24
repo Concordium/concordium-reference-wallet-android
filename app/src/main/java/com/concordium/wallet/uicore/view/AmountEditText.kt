f6082aec0d02f345f557d32c79c46a90e390d58c
Oleg Koretsky <oleg@radiokot.com.ua>
2023-05-24 13:45:45 +0300

    Fix AmountEditText not allowing decimal input in locales with ',' separator

--- app/src/main/java/com/concordium/wallet/uicore/view/AmountEditText.kt
+++ app/src/main/java/com/concordium/wallet/uicore/view/AmountEditText.kt
@@ -2,12 +2,13 @@ package com.concordium.wallet.uicore.view
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.text.InputType
 import android.text.method.DigitsKeyListener
 import android.util.AttributeSet
 import android.widget.EditText
 import com.concordium.wallet.uicore.DecimalTextWatcher
 import com.concordium.wallet.uicore.MaxAmountTextWatcher
-import java.text.DecimalFormatSymbols
+import java.util.*
 
 @SuppressLint("AppCompatCustomView")
 class AmountEditText : EditText {
@@ -33,9 +34,17 @@ class AmountEditText : EditText {
 
     @Suppress("UNUSED_PARAMETER")
     private fun init(attrs: AttributeSet?) {
-        keyListener =
-            DigitsKeyListener.getInstance("0123456789" + DecimalFormatSymbols.getInstance().decimalSeparator)
+        inputType = InputType.TYPE_CLASS_NUMBER
+
+        // All the possible decimal separators must be allowed.
+        // Keyboards do not respect the current locale's separator and send '.' no matter what.
+        // This, in combination with the default DigitsKeyListener or 'numberDecimal' input type,
+        // breaks decimal input in locales with ',' separator.
+        keyListener = DigitsKeyListener.getInstance("0123456789.,")
+
+        // This watcher fixes the improper decimal separator by replacing it with the proper one.
         addTextChangedListener(DecimalTextWatcher(6))
+
         addTextChangedListener(MaxAmountTextWatcher())
     }
 }
